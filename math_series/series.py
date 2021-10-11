aed4215ac91f95d57c38d76a472d8fc90e0eab6f
majedalswaeer <majedalswaeer69@gmail.com>
2021-10-11 22:53:25 +0300

    feat:finished lab02

--- math_series/series.py
+++ math_series/series.py
@@ -18,7 +18,9 @@ def lucas(n: int):
     for number 1 return the (n)th value which is 2 a lucas number
     for other nums will return the sum of the two numbers that precede it
     """
-    if n==0:
+    if n<0:
+        return("Enter a value more than zero")
+    elif n==0:
         return 2
     elif n==1:
         return 1
@@ -28,11 +30,24 @@ def lucas(n: int):
 def sum_series(n,x=0,y=1):
     """
     inputs for this function are three, one is required which is n and (x,y) which are optional
-    if a=1 and b=2, it will return the n(th) value of fibonacci
-    if a=2 and b=1, it will return the n(th) value of lucas
+    if x=1 and y=2, it will return the n(th) value of fibonacci
+    if x=2 and y=1, it will return the n(th) value of lucas
     if other values for the optional parameters, it will produce other series
-
     """
+    if n<0:
+        print('Enter a value more than zero')
+    if x==0 and y==1:
+        return fibonacci(n)
+    if x==2 and y==1:
+        return lucas(n)
+    if not x==0 or x==2:
+        return x
+    if not y==1:
+        return y
+
+    return(sum_series(n-1,x,y)+sum_series(n-2,x,y))
+    
+    
 
 
 
@@ -41,3 +56,6 @@ if __name__=='__main__':
     n=int(input('enter a number > '))
     print("fibonacci value >",fibonacci(n))
     print("lucas value >",lucas(n))
+    a=int(input('enter the first optional argument > '))
+    b=int(input('enter the second optional argument > '))
+    print(sum_series(n,a,b))
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,82 @@
+import pytest
 from math_series import __version__
+from math_series.series import fibonacci
+from math_series.series import lucas
+from math_series.series import sum_series
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_zero_fibonacci():
+    actual=fibonacci(0)
+    expected=0
+    assert actual==expected
+
+def test_one_fibonacci():
+    actual=fibonacci(1)
+    expected=1
+    assert actual==expected
+
+def test_two_fibonacci():
+    actual=fibonacci(2)
+    expected=1
+    assert actual==expected
+
+def test_three_fibonacci():
+    actual=fibonacci(3)
+    expected=2
+    assert actual==expected
+
+def test_four_fibonacci():
+    actual=fibonacci(4)
+    expected=3
+    assert actual==expected
+
+def test_ten_fibonacci():
+    actual=fibonacci(10)
+    expected=55
+    assert actual==expected 
+
+
+def test_zero():
+    actual=lucas(0)
+    expected=2
+    assert actual==expected
+
+def test_one():
+    actual=lucas(1)
+    expected=1
+    assert actual==expected
+
+def test_two():
+    actual=lucas(2)
+    expected=3
+    assert actual==expected
+
+def test_three():
+    actual=lucas(3)
+    expected=4
+    assert actual==expected
+
+def test_four():
+    actual=lucas(4)
+    expected=7
+    assert actual==expected
+
+def test_ten():
+    actual=lucas(10)
+    expected=123
+    assert actual==expected     
+
+
+def test_one_sum_series():
+    expected = 1
+    actual = sum_series(1,0,1)
+    assert expected == actual
+
+def test_two_sum_series():
+    expected = 3
+    actual = sum_series(2,2,1)
+    assert expected == actual
+
